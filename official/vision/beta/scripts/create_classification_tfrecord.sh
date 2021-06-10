a3b9fcb3e5fbe78983decdd717f598b4c85f125a
rehohoho <ernie.birdy.ho@gmail.com>
2021-06-10 19:28:48 +0800

    Standardise tfrecords naming.

--- official/vision/beta/scripts/create_classification_tfrecord.sh
+++ official/vision/beta/scripts/create_classification_tfrecord.sh
@@ -5,18 +5,18 @@ else
     export PYTHONPATH=$PYTHONPATH:${DATA_ENGINE_FOLDER}
 fi
 
-DATA_DIR="D:/data"
+DATA_DIR="/mnt/ssd2"
 DATASET_NAME="detector"
-DATASET_DIR="${DATA_DIR}/${DATASET_NAME}"
 INNER_DIR_PREFIX="detector"
 SUBSET="val"
+OUTPUT_PREFIX="cls_env"
 echo $DATASET_DIR
 
 set +o posix
 exec > >(tee create_img_tf_record_output.log) 2>&1
 python ../data/create_classification_tfrecord.py \
-  --image_dir="${DATASET_DIR}/${INNER_DIR_PREFIX}_images/${SUBSET}" \
-  --classes_json="${DATASET_DIR}/classes.json" \
-  --output_file_prefix="D:/data/whizz_tf/${DATASET_NAME}_${SUBSET}" \
+  --image_dir="${DATA_DIR}/${DATASET_NAME}/${INNER_DIR_PREFIX}_images/${SUBSET}" \
+  --classes_json="${DATA_DIR}/${DATASET_NAME}/classes.json" \
+  --output_file_prefix="/mnt/ssd2/tfrecords/${OUTPUT_PREFIX}_${SUBSET}_${DATASET_NAME}" \
   --json_key="path_type" \
-  --num_shards=8
+  --num_shards=4
