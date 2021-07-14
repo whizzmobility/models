c95cde9251ba693ca3c3737fc3ca15cb4040d67c
rehohoho <ernie.birdy.ho@gmail.com>
2021-07-14 17:53:00 +0800

    Update multitask serving definition.

--- official/vision/beta/serving/multitask.py
+++ official/vision/beta/serving/multitask.py
@@ -1,15 +1,16 @@
 # Lint as: python3
 """Semantic segmentation input and model functions for serving/inference."""
 
-from typing import Mapping
+from typing import Mapping, Callable
 
-from absl import logging
+import numpy as np
 import tensorflow as tf
 
 from official.vision.beta.modeling import factory_multitask
-from official.vision.beta.ops import preprocess_ops
+from official.vision.beta.ops import preprocess_ops, yolo_ops, box_ops
 from official.vision.beta.ops.colormaps import get_colormap
-from official.vision.beta.serving import export_base
+from official.vision.beta.serving import export_base, run_lib
+from official.vision.beta.projects.yolo.ops import box_ops as yolo_box_ops
 
 # RGB mean and stddev from ImageNet
 MEAN_RGB = (0.485 * 255, 0.456 * 255, 0.406 * 255)
@@ -49,7 +50,8 @@ class MultitaskModule(export_base.ExportModule):
         self._input_image_size,
         padded_size=self._input_image_size,
         aug_scale_min=1.0,
-        aug_scale_max=1.0)
+        aug_scale_max=1.0,
+        preserve_aspect_ratio=False)
     return image
 
   def serve(self, images: tf.Tensor) -> Mapping[str, tf.Tensor]:
@@ -85,17 +87,56 @@ class MultitaskModule(export_base.ExportModule):
     
     for name, output in outputs.items():
       
-      if len(output.shape) == 4:
-        output = tf.image.resize(
-          output, self._input_image_size, method='bilinear')
-      
-      if self._argmax_outputs:
-        output = tf.math.argmax(output, -1)
-      processed_outputs[name] = output
+      if 'classification' in name:
+        if self._argmax_outputs:
+          output = tf.math.argmax(output, -1)
+        else:
+          output = tf.nn.softmax(output)
+        processed_outputs[name] = output
 
-      if self._visualise_outputs and len(output.shape) == 3:
-        colormap = get_colormap(cmap_type='cityscapes_int')
-        processed_outputs[name + '_visualised'] = tf.gather(
-          colormap, tf.cast(tf.squeeze(output), tf.int32))
+      elif 'segmentation' in name:
+        output = tf.image.resize(
+          output, self._input_image_size, method='bilinear')      
+        
+        if self._argmax_outputs:
+          output = tf.math.argmax(output, -1)
+        processed_outputs[name] = output
+        
+        if self._visualise_outputs and len(output.shape) == 3:
+          colormap = get_colormap(cmap_type='cityscapes_int')
+          processed_outputs[name + '_visualised'] = tf.gather(
+            colormap, tf.cast(tf.squeeze(output), tf.int32))
+      
+      elif 'yolo' in name:
+        num_classes = output['predictions']['0'].shape[-1] - 5
+        bbox_tensors, prob_tensors = yolo_ops.concat_tensor_dict(
+          tensor_dict=output['predictions'], 
+          num_classes=num_classes
+        )
+
+        boxes = tf.concat(bbox_tensors, axis=1)
+        boxes = tf.squeeze(yolo_box_ops.xcycwh_to_yxyx(boxes))
+        scores = tf.concat(prob_tensors, axis=1)
+        scores = tf.squeeze(tf.math.reduce_max(scores, axis=-1))
+        classes = tf.argmax(prob_tensors, axis=-1)
+        
+        indices = tf.image.non_max_suppression(boxes=boxes,
+                                               scores=scores,
+                                               max_output_size=20,
+                                               iou_threshold=0.5,
+                                               score_threshold=0.25)
+        
+        boxes = tf.expand_dims(tf.gather(boxes, indices), axis=0)
+        boxes = box_ops.normalize_boxes(boxes, self._input_image_size)
+        scores = tf.expand_dims(tf.gather(scores, indices), axis=0)
+        classes = tf.gather(classes, indices, axis=1)
+
+        processed_outputs[name + 'boxes'] = boxes
+        processed_outputs[name + 'classes'] = classes
+        processed_outputs[name + 'scores'] = scores
+
+      else:
+        raise NotImplementedError('Task type %s is not implemented.' + \
+          'Try renaming the task routine.' %name)
 
     return processed_outputs
