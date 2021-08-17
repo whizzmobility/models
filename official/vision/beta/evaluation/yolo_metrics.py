de96d02c516d8b5d24dec341c96b770c5ced9f21
rehohoho <ernie.birdy.ho@gmail.com>
2021-08-17 15:37:00 +0800

    Refactor out repeated code. Update method docs.

--- official/vision/beta/evaluation/yolo_metrics.py
+++ official/vision/beta/evaluation/yolo_metrics.py
@@ -7,9 +7,14 @@ import tensorflow as tf
 from official.vision.beta.ops import yolo_ops
 
 
-class AveragePrecisionAtIou(tf.keras.metrics.Precision):
-  """Compute Average Precision at given box IOU for YOLO
-  Precision is defined as: true_positives / (true_positives + false_positives)
+def applyIouAndConfThreshold(y_true: tf.Tensor,
+                             y_pred: tf.Tensor,
+                             num_classes: int,
+                             conf_thres: float,
+                             iou_thres: float):
+  """
+  Alter prediction and groundtruth tensors for update_state, according to confidence
+  and iou threshold.
 
   COCO PrecisionAtIou logic:
     for each detection that has a confidence score > threshold:
@@ -22,7 +27,46 @@ class AveragePrecisionAtIou(tf.keras.metrics.Precision):
         the detection is a true positive
   
   YoloV4 training is anchor boxes based, we choose the corresponding anchor box
-  and coordinate position, for each detection, with sufficient confidence instead.
+  and coordinate position, for each detection, with sufficient confidence instead. Hence,
+    1) set predictions to false when not enough confidence
+    2) set groundtruth to false when enough confidence and not enough IoU
+
+  TP: (1) enough confidence, (2) same class, (3) enough IoU
+  FP: (1) enough confidence, (2) either wrong class or not enough IoU
+  FN: (1) not enough confidence, (2) same class
+  TN: (1) not enough confidence, (2) otherwise
+
+  Args:
+    y_true: The ground truth values, shape (batch, yind, xind, anchorind, xywh/conf/classes)
+    y_pred: The predicted values, shape (batch, yind, xind, anchorind, xywh/conf/classes)
+  """
+
+  bbox_true, _, prob_true = yolo_ops.concat_tensor_dict(y_true, num_classes)
+  bbox_pred, conf_pred, prob_pred = yolo_ops.concat_tensor_dict(y_pred, num_classes)
+
+  conf_mask = conf_pred >= conf_thres
+  conf_mask = tf.expand_dims(conf_mask, -1)
+  iou = yolo_ops.bbox_iou(bbox_true, bbox_pred)
+  lack_iou_mask = iou < iou_thres
+  lack_iou_mask = tf.expand_dims(lack_iou_mask, -1)
+
+  prob_pred = tf.argmax(prob_pred, axis=-1)
+  prob_pred = tf.one_hot(prob_pred, num_classes, on_value=True, off_value=False)
+  # retain predictions with sufficient confidence
+  prob_pred = tf.where(conf_mask, prob_pred, tf.fill([num_classes], False))
+  
+  prob_true = tf.argmax(prob_true, axis=-1)
+  prob_true = tf.one_hot(prob_true, num_classes, on_value=True, off_value=False)
+  conf_and_no_iou_mask = tf.math.logical_and(conf_mask, lack_iou_mask)
+  # remove ground truths with sufficient prediction confidence, not enough IoU
+  prob_true = tf.where(conf_and_no_iou_mask, tf.fill([num_classes], False), prob_true)
+
+  return prob_true, prob_pred
+
+
+class AveragePrecisionAtIou(tf.keras.metrics.Precision):
+  """Compute Average Precision at given box IOU for YOLO
+  Precision is defined as: true_positives / (true_positives + false_positives)
 
   The predictions are accumulated in a confusion matrix, weighted by `sample_weight` 
   and the metric is then calculated from it. Weights default to 1.
@@ -57,7 +101,7 @@ class AveragePrecisionAtIou(tf.keras.metrics.Precision):
 
     Args:
       y_true: The ground truth values, shape (batch, yind, xind, anchorind, xywh/conf/classes)
-      y_pred: The predicted values.
+      y_pred: The predicted values, shape (batch, yind, xind, anchorind, xywh/conf/classes)
       sample_weight: Optional weighting of each example. Defaults to 1. Can be a
         `Tensor` whose rank is either 0, or the same rank as `y_true`, and must
         be broadcastable to `y_true`.
@@ -65,37 +109,11 @@ class AveragePrecisionAtIou(tf.keras.metrics.Precision):
     Returns:
       Overall Precision at IoU threshold
     """
-
-    bbox_true, _, prob_true = yolo_ops.concat_tensor_dict(
-      tensor_dict=y_true,
-      num_classes=self.num_classes)
-    bbox_pred, conf_pred, prob_pred = yolo_ops.concat_tensor_dict(
-      tensor_dict=y_pred,
-      num_classes=self.num_classes)
-
-    conf_mask = conf_pred >= self.conf_thres
-    conf_mask = tf.expand_dims(conf_mask, -1)
-    iou = yolo_ops.bbox_iou(bbox_true, bbox_pred)
-    lack_iou_mask = iou < self.iou_thres
-    lack_iou_mask = tf.expand_dims(lack_iou_mask, -1)
-
-    # set predictions to false when not enough confidence
-    # set groundtruth to false when enough confidence and not enough IoU
-    #   TP: (1) enough confidence, (2) same class, (3) enough IoU
-    #   FP: (1) enough confidence, (2) either wrong class or not enough IoU
-    #   FN: (1) not enough confidence, (2) same class
-    #   TN: (1) not enough confidence, (2) otherwise
-
-    prob_pred = tf.argmax(prob_pred, axis=-1)
-    prob_pred = tf.one_hot(prob_pred, self.num_classes, on_value=True, off_value=False)
-    # retain predictions with sufficient confidence
-    prob_pred = tf.where(conf_mask, prob_pred, tf.fill([self.num_classes], False))
-    
-    prob_true = tf.argmax(prob_true, axis=-1)
-    prob_true = tf.one_hot(prob_true, self.num_classes, on_value=True, off_value=False)
-    conf_and_no_iou_mask = tf.math.logical_and(conf_mask, lack_iou_mask)
-    # remove ground truths with sufficient prediction confidence, not enough IoU
-    prob_true = tf.where(conf_and_no_iou_mask, tf.fill([self.num_classes], False), prob_true)
+    prob_true, prob_pred = applyIouAndConfThreshold(y_true=y_true,
+                                                    y_pred=y_pred,
+                                                    num_classes=self.num_classes,
+                                                    conf_thres=self.conf_thres,
+                                                    iou_thres=self.iou_thres)
 
     return super(AveragePrecisionAtIou, self).update_state(
         y_true=prob_true, y_pred=prob_pred, sample_weight=None)
@@ -104,19 +122,6 @@ class AveragePrecisionAtIou(tf.keras.metrics.Precision):
 class AverageRecallAtIou(tf.keras.metrics.Recall):
   """Compute Average Recall at given box IOU for YOLO
   Recall is defined as: true_positives / (true_positives + false_negative)
-  
-  COCO PrecisionAtIou logic:
-    for each detection that has a confidence score > threshold:
-      among the ground-truths, choose one that belongs to the same class 
-      and has the highest IoU with the detection
-      
-      if no ground-truth can be chosen or IoU < threshold (e.g., 0.5):
-        the detection is a false positive
-      else:
-        the detection is a true positive
-  
-  YoloV4 training is anchor boxes based, we choose the corresponding anchor box
-  and coordinate position, for each detection, with sufficient confidence instead.
 
   The predictions are accumulated in a confusion matrix, weighted by `sample_weight` 
   and the metric is then calculated from it. Weights default to 1.
@@ -149,8 +154,8 @@ class AverageRecallAtIou(tf.keras.metrics.Recall):
     """Accumulates the confusion matrix statistics.
 
     Args:
-      y_true: The ground truth values.
-      y_pred: The predicted values.
+      y_true: The ground truth values, shape (batch, yind, xind, anchorind, xywh/conf/classes)
+      y_pred: The predicted values, shape (batch, yind, xind, anchorind, xywh/conf/classes)
       sample_weight: Optional weighting of each example. Defaults to 1. Can be a
         `Tensor` whose rank is either 0, or the same rank as `y_true`, and must
         be broadcastable to `y_true`.
@@ -158,36 +163,11 @@ class AverageRecallAtIou(tf.keras.metrics.Recall):
     Returns:
       Overall Recall, filtered at given IoU.
     """
-    bbox_true, _, prob_true = yolo_ops.concat_tensor_dict(
-      tensor_dict=y_true,
-      num_classes=self.num_classes)
-    bbox_pred, conf_pred, prob_pred = yolo_ops.concat_tensor_dict(
-      tensor_dict=y_pred,
-      num_classes=self.num_classes)
-
-    conf_mask = conf_pred >= self.conf_thres
-    conf_mask = tf.expand_dims(conf_mask, -1)
-    iou = yolo_ops.bbox_iou(bbox_true, bbox_pred)
-    lack_iou_mask = iou < self.iou_thres
-    lack_iou_mask = tf.expand_dims(lack_iou_mask, -1)
-
-    # set predictions to false when not enough confidence
-    # set groundtruth to false when enough confidence and not enough IoU
-    #   TP: (1) enough confidence, (2) same class, (3) enough IoU
-    #   FP: (1) enough confidence, (2) either wrong class or not enough IoU
-    #   FN: (1) not enough confidence, (2) same class
-    #   TN: (1) not enough confidence, (2) otherwise
-
-    prob_pred = tf.argmax(prob_pred, axis=-1)
-    prob_pred = tf.one_hot(prob_pred, self.num_classes, on_value=True, off_value=False)
-    # retain predictions with sufficient confidence
-    prob_pred = tf.where(conf_mask, prob_pred, tf.fill([self.num_classes], False))
-    
-    prob_true = tf.argmax(prob_true, axis=-1)
-    prob_true = tf.one_hot(prob_true, self.num_classes, on_value=True, off_value=False)
-    conf_and_no_iou_mask = tf.math.logical_and(conf_mask, lack_iou_mask)
-    # remove ground truths with sufficient prediction confidence, not enough IoU
-    prob_true = tf.where(conf_and_no_iou_mask, tf.fill([self.num_classes], False), prob_true)
+    prob_true, prob_pred = applyIouAndConfThreshold(y_true=y_true,
+                                                    y_pred=y_pred,
+                                                    num_classes=self.num_classes,
+                                                    conf_thres=self.conf_thres,
+                                                    iou_thres=self.iou_thres)
 
     return super(AverageRecallAtIou, self).update_state(
         y_true=prob_true, y_pred=prob_pred, sample_weight=None)
